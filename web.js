f579c9f1168ac42ace2d920fabfd4ce0e2940a5d
bnavaneethan <bharat_narra@yahoo.com>
2013-07-16 07:14:11 +0000

    Update web.js

--- web.js
+++ web.js
@@ -4,8 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  // response.send('Hello World - Bharath');
-  fs.readFileSync('bitstarter/index.html');
+  fs.readFileSync('index.html');
 });
 
 var port = process.env.PORT || 5000;
