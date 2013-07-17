ac2aeafd7fca5e2e7a20bd81ea6eafefa5581dd7
bnavaneethan <bharat_narra@yahoo.com>
2013-07-17 11:54:28 +0000

    Updated version of web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
+var fs = require('fs');
 var express = require('express');
+var INDEX_FILE = './index.html';
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(fs.readFileSync(INDEX_FILE));
 });
 
 var port = process.env.PORT || 5000;
