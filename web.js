c68cd1eac3859a9fd3c21f38e004b129c13472ec
bnavaneethan <bharat_narra@yahoo.com>
2013-07-17 06:58:03 +0000

    Updated

--- web.js
+++ web.js
@@ -2,11 +2,12 @@ var express = require('express');
 var fs = require('fs');
 var buf = new Buffer();
 var json = JSON.stringify(buf);
+var index_file = 'index.html';
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync(index.html)));
+  response.send(buf.toString(fs.readFileSync(index_file)));
 });
 
 var port = process.env.PORT || 5000;
