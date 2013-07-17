ee666409d53b7b247f9b0efa75f7387b91c265ae
bnavaneethan <bharat_narra@yahoo.com>
2013-07-17 11:48:48 +0000

    Old version of web.js

--- web.js
+++ web.js
@@ -1,13 +1,9 @@
 var express = require('express');
-var fs = require('fs');
-var buf = new Buffer();
-var json = JSON.stringify(buf);
-var index_file = 'index.html';
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString(fs.readFileSync(index_file)));
+  response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
